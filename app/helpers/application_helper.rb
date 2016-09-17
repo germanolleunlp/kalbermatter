31a54ddf2ac00dd944215eb045bb1566c225d729
German Nicolas Olle <germanolle.unlp@gmail.com>
2016-09-17 19:00:46 -0300

    devise users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_div(level)
+    if flash[level].present?
+      # level directly corresponds to rails types for 2/3 bootstrap styles.
+      # Info notice needs conversion.
+      classes = "alert alert-#{level}" + (level == "notice" ? "alert-info" : "")
+      content_tag "div", class: classes do
+        content_tag "p", flash[level]
+      end
+    end
+  end
 end
