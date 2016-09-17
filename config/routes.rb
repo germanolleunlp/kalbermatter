d5faf6dc1ed3e42a2558952ba30e2c38626af4d5
German Nicolas Olle <germanolle.unlp@gmail.com>
2016-09-17 20:23:37 -0300

    simplify user devise options

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'welcome#index'
+
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    passwords: "users/passwords"
+  }
 end
