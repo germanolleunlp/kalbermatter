67f71c3e71a57b5b37a6b0d4daf14d1d0db2cf47
German Nicolas Olle <germanolle.unlp@gmail.com>
2016-10-10 10:32:17 -0300

    Remove registrable from user

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Kalbermatter::Application.routes.draw do
   root 'welcome#index'
 
   devise_for :users, controllers: {
-    registrations: "users/registrations",
-    sessions: "users/sessions",
-    passwords: "users/passwords"
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
   }
 end
