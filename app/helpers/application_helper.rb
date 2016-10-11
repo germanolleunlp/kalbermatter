27a8a1988af2cc8ab07b0f2b00aaf3b660e95b0c
German Nicolas Olle <germanolle.unlp@gmail.com>
2016-10-11 01:57:26 -0300

    update flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,8 @@ module ApplicationHelper
     if flash[level].present?
       # level directly corresponds to rails types for 2/3 bootstrap styles.
       # Info notice needs conversion.
-      classes = "alert alert-#{(level.to_sym == :notice ? 'info' : level)}"
+      alert_class = [:notice, :alert].include?(level.to_sym) ? 'info' : level
+      classes = "alert alert-#{alert_class}"
       content_tag 'div', class: classes do
         content_tag 'p', flash[level]
       end
